fa03ce29a88143979ab5a4cc43e93d58e71c230e
SturmEnte <sturmente@proton.me>
2024-03-10 23:41:57 +0100

    Added morphing effect to text

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-const foregroundElement = document.getElementById("foreground");
+/*const foregroundElement = document.getElementById("foreground");
 
 let i = 0;
 const parts = ["What", "is", "Art?"];
 
 setInterval(() => {
-	foregroundElement.innerHTML = parts[i];
+	let currentChars = parts[i].split();
+
+
+
 	i++;
 	if (i >= parts.length) i = 0;
 }, 2000);
+*/
