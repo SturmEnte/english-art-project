aa4463022a5ef9a38afb1c0a2b4835344e0580ca
SturmEnte <sturmente@proton.me>
2024-03-10 22:41:44 +0100

    Imported Roboto Mono and done with first page

---
+++ main.js
@@ -0,0 +1,12 @@
+const container = document.querySelector("#wrapper");
+const slides = container.querySelectorAll(".slide");
+
+window.addEventListener("scroll", () => {
+	let scrollPosition = window.scrollY;
+	let snappedSlideIndex = Math.floor(scrollPosition / slides[0].clientHeight);
+
+	container.scrollTo({
+		top: snappedSlideIndex * slides[0].clientHeight,
+		behavior: "smooth",
+	});
+});
